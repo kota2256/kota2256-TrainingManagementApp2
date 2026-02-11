391a4c47e5b574b52afec5d2271550ccd2c19263
koto <koto@localhost>
2026-02-11 22:52:25 +0900

    初期コミット

---
+++ TrainingManagementApp2/src/main/java/com/example/TrainingManagementApp2.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingManagementApp2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingManagementApp2.class, args);
+	}
+
+	
+}
