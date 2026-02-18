f16f7b16ebb0f87e7a509a83630e7b8ac2e6f340
koto <koto@localhost>
2026-02-19 00:15:06 +0900

    260218_ホーム（パス変更）
    ・画面遷移先のパスを指定パスに変更

--- TrainingManagementApp2/src/main/java/com/example/controller/HomeController.java
+++ TrainingManagementApp2/src/main/java/com/example/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.model.MUser;
+import com.example.service.TMService;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	TMService tmService;
+	
 	@GetMapping("/home")
-	public String getHome() {
+	public String getHome(Model model,@AuthenticationPrincipal UserDetails loginuser) {
+		
+		MUser user = tmService.getLoginUser(loginuser.getUsername());
+		model.addAttribute("id", user.getId());
+		
 		return "home";
 	}
 }
--- TrainingManagementApp2/src/main/java/com/example/controller/WeightLogController.java
+++ TrainingManagementApp2/src/main/java/com/example/controller/WeightLogController.java
@@ -2,20 +2,12 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
 public class WeightLogController {
 
-	@GetMapping("/weightLog")
+	@GetMapping("/physicals/create")
 	public String getWeightLog() {
 		return "user/weightLog";
-	}
-	
-	@PostMapping("/weightLog")
-	public String postWeightLog() {
-		return "redirect:/user/weightLog";
 	}
 }
