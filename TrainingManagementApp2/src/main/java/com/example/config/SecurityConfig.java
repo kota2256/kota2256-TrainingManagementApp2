6d5abf9e59d86c18df9eb907a4f2329ffb691d47
koto <koto@localhost>
2026-02-20 14:45:47 +0900

    260220_ログイン（バリデーション実装）

--- TrainingManagementApp2/src/main/java/com/example/config/SecurityConfig.java
+++ TrainingManagementApp2/src/main/java/com/example/config/SecurityConfig.java
@@ -1,4 +1,5 @@
 package com.example.config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 
 @Configuration				// 設定ファイルであることを示すアノテーション
 @EnableWebSecurity	//カスタムセキュリティ設定を有効（デフォルトのセキュリティ設定が無効）にする
--- TrainingManagementApp2/src/main/java/com/example/controller/LoginController.java
+++ TrainingManagementApp2/src/main/java/com/example/controller/LoginController.java
@@ -1,19 +1,34 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.form.LoginForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class LoginController {
 
 	@GetMapping("/login")
-	public String getLogin() {
+	public String getLogin(@ModelAttribute LoginForm form) {
 		return "login";
 	}
 	
-	//SecurityConfigで設定済み
-//	@PostMapping("login")
-//	public String postLogin() {
-//		return "redirect:/home";
-//	}
+//	SecurityConfigで設定済み
+	@PostMapping("login")
+	public String postLogin(@ModelAttribute LoginForm form, BindingResult bindingresult) {
+		
+		if(bindingresult.hasErrors()) {
+			return getLogin(form);
+		}
+		
+		log.info(form.toString());
+		
+		return "redirect:/home";
+	}
 }
