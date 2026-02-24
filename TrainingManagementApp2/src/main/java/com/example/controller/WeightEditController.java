a6de1d0e1642678436d4d71d16665309bc3082ff
koto <koto@localhost>
2026-02-24 23:25:04 +0900

    260224_体重更新（バリデーション）

--- TrainingManagementApp2/src/main/java/com/example/controller/WeightEditController.java
+++ TrainingManagementApp2/src/main/java/com/example/controller/WeightEditController.java
@@ -5,18 +5,23 @@ import java.util.Date;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.form.WeightEditForm;
 import com.example.model.WeightLog;
 import com.example.service.TMService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j		//Simple Logging Facade for Javaの略
 public class WeightEditController {
 
 	@Autowired
@@ -25,28 +30,33 @@ public class WeightEditController {
 	private ModelMapper modelMapper;
 	
 	@GetMapping("/physicals/edit/physical_details.{recodedDate}")
-	public String getEditForm(Model model, WeightEditForm form,
+	public String getEditForm(Model model, WeightEditForm form, 
 			@RequestParam("userId") int userId,
-			@RequestParam("createdAt") @DateTimeFormat(pattern = "EEE MMM dd HH:mm:ss zzz yyyy") Date createdAt,
-			@AuthenticationPrincipal UserDetails loginuser) {
+			@RequestParam("createdAt") @DateTimeFormat(pattern = "EEE MMM dd HH:mm:ss zzz yyyy") Date createdAt) {
 		
 		// 体重記録一件取得
 		WeightLog weightLogOne = tmService.getWeightLogOne(userId, createdAt);
 		// 画面表示用にモデル格納
 		form = modelMapper.map(weightLogOne, WeightEditForm.class);
 		model.addAttribute("weightEditForm", form);
-		
 		return "user/weightEdit";
 	}
 	
-//	@PostMapping("/physicals/edit/physical_details.{recodedDate}")
-//	public String postEditForm(@ModelAttribute WeightEditForm form, @PathVariable("createdAt") String createdAt, @AuthenticationPrincipal UserDetails loginuser) {
-//		
-//		// 更新処理
-//		
-//		// 体重閲覧画面にリダイレクト
-//		MUser user = tmService.getLoginUser(loginuser.getUsername());
-//		return "redirect:/physicals/users." + user.getId();
-//	}
-//	
+	@PostMapping("/update")
+	public String postEditForm(Model model, @ModelAttribute @Validated WeightEditForm form, BindingResult bindingresult) {
+		
+		// エラーチェック
+		if (bindingresult.hasErrors()) {
+			return "user/weightEdit";
+		}
+		
+		log.info(form.toString());
+		
+		// 更新処理 
+		
+		
+		// 体重閲覧画面にリダイレクト
+		return "redirect:/physicals/users." + form.getUserId();
+	}
+	
 }
--- TrainingManagementApp2/src/main/java/com/example/form/WeightEditForm.java
+++ TrainingManagementApp2/src/main/java/com/example/form/WeightEditForm.java
@@ -11,10 +11,16 @@ import lombok.Data;
 
 @Data
 public class WeightEditForm {
+	private int userId;
+	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@NotNull
 	private Date recordedDate;
+	
 	@Digits(integer=3, fraction=1)
 	@NotNull
 	private Double weight;
+	
+	@DateTimeFormat(pattern = "EEE MMM dd HH:mm:ss zzz yyyy") 
+	private Date createdAt;
 }
